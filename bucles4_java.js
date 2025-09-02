3ee084fcd779d0dcaacdfccbe36e5747fa71a83e
d3lac0baa <sercoga2006@gmail.com>
2025-09-03 01:01:29 +0200

    upload

---
+++ bucles4_java.js
@@ -0,0 +1,24 @@
+const cartoons = [
+  { name: 'Mickey Mouse', debut: 1928 },
+  { name: 'Bugs Bunny', debut: 1938 },
+  { name: 'SpongeBob SquarePants', debut: 1999 },
+  { name: 'Tom and Jerry', debut: 1940 },
+  { name: 'Scooby-Doo', debut: 1969 },
+  { name: 'The Flintstones', debut: 1960 },
+  { name: 'Batman: The Animated Series', debut: 1992 },
+  { name: 'The Simpsons', debut: 1989 },
+  { name: 'Pokémon', debut: 1997 },
+  { name: "Dexter's Laboratory", debut: 1996 }
+];
+
+let oldestCartoonName = '';
+let oldestDebutYear = Infinity;
+
+for (const cartoon of cartoons) {
+  if (cartoon.debut < oldestDebutYear) {
+    oldestDebutYear = cartoon.debut;
+    oldestCartoonName = cartoon.name;
+  }
+}
+
+console.log(oldestCartoonName);
